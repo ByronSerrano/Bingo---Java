d2b5127f2423e30ab7fa0644f459d1a966410881
Byron_Sr <115562510+ByronSerrano@users.noreply.github.com>
2022-12-02 22:34:28 -0500

    Agregando Bingo75, y todo el bingo actualizado
    Se le añadió el Bingo75 más el cartón.

---
+++ bingo75/Main.java
@@ -0,0 +1,13 @@
+package bingo75;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Bingo bingo75 = new Bingo75();
+		bingo75.jugar();
+
+	}
+
+}
